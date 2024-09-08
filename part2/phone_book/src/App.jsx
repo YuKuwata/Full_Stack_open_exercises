246ccfc666ca3eaefb715c6caf9b730d86d3bde7
Yu <13011346529@163.com>
2024-09-08 23:20:09 +0800

    2.9

--- part2/phone_book/src/App.jsx
+++ part2/phone_book/src/App.jsx
@@ -4,17 +4,27 @@ import { some } from 'lodash-es'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [shownPersons, setShownPersons] = useState(persons);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input onChange={(event) => {
+          if (!event.target.value) {
+            setShownPersons(persons);
+          }
+          setShownPersons(persons.filter(person => person.name.toUpperCase().includes(event.target.value.toUpperCase())))
+        }} />
+      </div>
       <form>
         <div>
           name: <input onChange={(event) => setNewName(event.target.value)} />
@@ -40,7 +50,7 @@ const App = () => {
       </form >
       <h2>Numbers</h2>
       {
-        persons.map((person) => <div key={person.name}>{person.name}&nbsp;{person.number}</div>)
+        shownPersons.map((person) => <div key={person.name}>{person.name}&nbsp;{person.number}</div>)
       }
     </div >
   )
